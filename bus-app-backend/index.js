819460f941438d6efe636886070c4e2e3c258476
Jenin-Immanuel <jeninimmi2003@gmail.com>
2024-04-20 17:01:59 +0530

    Add PRODUCTION_URL for cors support

--- bus-app-backend/index.js
+++ bus-app-backend/index.js
@@ -21,11 +21,11 @@ const errorHandler = require("./middlewares/errorHandler")
 const app = express()
 
 const PORT = process.env.PORT || 8080
-
+const PRODUCTION_URL = process.env.PRODUCTION_FRONTEND_URL
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true, limit: "10mb" }))
 app.use(cookieParser())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({ origin: PRODUCTION_URL, credentials: true }))
 
 async function main() {
   connectDB()
